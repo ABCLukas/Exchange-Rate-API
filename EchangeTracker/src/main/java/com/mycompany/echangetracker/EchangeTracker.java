64d0ac9c62260d08d8e4b84561fd44e77e09c235
Lukas <153985968+ABCLukas@users.noreply.github.com>
2024-01-29 20:49:19 +0100

    Add files via upload

---
+++ EchangeTracker/src/main/java/com/mycompany/echangetracker/EchangeTracker.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.echangetracker;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ *
+ * @author schne
+ */
+public class EchangeTracker {
+
+    public static void main(String[] args) {
+        //Create standart classes
+        String api_key = "Your API KEY";
+        String baseCurrency = "USD";
+        String targetCurrency = "CHF";
+        
+        //Build the URL
+        String apiUrl = "https://open.er-api.com/v6/latest/" + baseCurrency + "?apikey=" + api_key;
+        //Create a Http Client
+        HttpClient client = HttpClient.newHttpClient();
+        //Create Request
+        HttpRequest request = HttpRequest.newBuilder()
+               .uri(URI.create(apiUrl))
+               .GET()
+               .build();
+        try {
+            // Send the request and get the response
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            // Check if successful (code 200)
+            if (response.statusCode() == 200) {
+                String responseBody = response.body();
+                //Using gson
+                JsonObject jsonObject = JsonParser.parseString(responseBody).getAsJsonObject();
+
+                double exchangeRate = jsonObject.getAsJsonObject("rates").get(targetCurrency).getAsDouble();
+
+                System.out.println("Exchange rate from " + baseCurrency + " to " + targetCurrency + ": " + exchangeRate);
+            } else {
+                System.out.println("Error: " + response.statusCode() + " - " + response.body());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
